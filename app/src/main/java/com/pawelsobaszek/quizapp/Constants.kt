6c1cfb8d834cf9b1404085f46feee83edd3a788e
Paweł Sobaszek <szczowio@gmail.com>
2020-05-30 09:47:37 +0200

    second commmit

--- app/src/main/java/com/pawelsobaszek/quizapp/Constants.kt
+++ app/src/main/java/com/pawelsobaszek/quizapp/Constants.kt
@@ -1,6 +1,12 @@
 package com.pawelsobaszek.quizapp
 
 object Constants{
+
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_question"
+    const val CORRECT_ANSWERS: String = "correct_answers"
+
+
     fun getQuestions(): ArrayList<Question>{
         val questionsList = ArrayList<Question>()
 
---
+++ app/src/main/java/com/pawelsobaszek/quizapp/ResultActivity.kt
@@ -0,0 +1,26 @@
+package com.pawelsobaszek.quizapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val username = intent.getStringExtra(Constants.USER_NAME)
+        tv_name.text = username
+        val totalQustions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tv_score.text = "Your Score is $correctAnswers out of $totalQustions"
+
+        btn_finish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
